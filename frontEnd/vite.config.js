bd00ea6180837d7c563ba05bdd6e5a45795a3294
vmedia7860-pixel <vmedia7860@gmail.com>
2025-12-05 22:35:25 +0530

    Update vite.config.js

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/civic-issue-reporting/',
   plugins: [react()],
   server: {
     port: 3000,
     open: true
   }
 })
-
